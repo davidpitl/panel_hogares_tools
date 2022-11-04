97c53649b55d1afc395352af0ff6bfd51a750e1e
Ricardo <gricardo.martin@gmail.com>
2022-11-04 09:33:28 +0100

    Cambios desde PC de Ricardo

---
+++ out_unificado/2019/paste_cmd2019.sh
@@ -0,0 +1,3 @@
+# dos2unix *.txt
+# dos2unix *.TXT
+paste -d"\0"  1_IDEN2016.txt 2_Renta2016.txt 3_RentaImputacion2016.txt 5_Patrimonio2016.txt 7_M190_2016.txt 1_IDEN2017.txt 2_Renta2017.txt 3_RentaImputacion2017.txt 5_Patrimonio2017.txt 7_M190_2017.txt 1_IDEN2018.txt 2_Renta2018.txt 3_RentaImputacion2018.txt 5_Patrimonio2018.txt 7_M190_2018.txt 1_IDEN2019.txt 2_Renta2019.txt 3_RentaImputacion2019.txt 5_Patrimonio2019.txt 7_M190_2019.txt > 2019_unificado.txt
